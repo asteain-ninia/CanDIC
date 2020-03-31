60b3b7e52725b418a3c3edb2a5de3e98b3b7cece
asteain-ninia <asteain25@gmail.com>
2020-03-31 14:59:54 +0900

    実験用要素の削除・読み込みの基礎生成

--- js/script_1.js
+++ js/script_1.js
@@ -1,72 +1,16 @@
 const electron = require('electron');
-const {ipcRenderer}=require( 'electron');
-const {dialog}=require('electron').remote;
+const {ipcRenderer}=electron;
 const fs=require('fs')
 
-ipcRenderer.on('open_file', (event, arg) => {
-//参考：https://qiita.com/stupid_student2/items/f25c2b8c3d0ca5cdc89e
-    console.log("load Start");
-    console.log(arg);
-/*     let filePath=arg[0];
-    filePath=filePath.split('\\');
-    const fileName =filePath[filePath.length-1];;
-    dialog.showMessageBox(fileName); */
-  })
-
-ipcRenderer.on('file_signal',function(event,arg){console.log(arg);})
-
-function  message(){
-  const result =ipcRenderer.sendSync('3','ping')
-  console.log(result)
-
-}
-
-ipcRenderer.on('1',(event,arg)=>{console.log(arg)}) 
-
-//以下参考:https://qiita.com/zaburo/items/eb525138b88890c5357c
 const editor = document.getElementById('editor');
-//openFileボタンが押されたとき（ファイル名取得まで）
 
-function openFile() {
-  var result =dialog.showOpenDialogSync({
-    properties: ['openFile'],
-    filters: [
-      {name: 'JSONファイル', extensions: ['json']}
-    ]})
-  console.log (result)
-  readFile(result)
-}
-
-function readFile(path) {
-  fs.readFile(path, (error, data) => {
-      if (error != null) {
-          alert("file open error.");
-          return;
-      }
-    editor.textContent = data.toString();
-  })
-}
-
-//https://www.sejuku.net/blog/32532
-var form = document.forms.myform;
-form.myfile.addEventListener( 'change', function test(e) {
-  console.log(e.target.files[0])
-  result=e.target.files[0];
-  var reader=new FileReader();
-  reader.readAsText(result);
-  reader.addEventListener( 'load', function() {
-    
-    console.log( JSON.parse(reader.result) );
-    
-})
-})
-
-
+var json = null;
 
 ipcRenderer.on('4',function(event,arg){
   console.log("filePath Reserved!")
   var result=arg[0];
   console.log(result)
   var data=fs.readFileSync(result,'utf8')
-  console.log(JSON.parse(data.words[0]));
+  json =JSON.parse(data);
+  console.log(json.words[0].entry.id);
 })
