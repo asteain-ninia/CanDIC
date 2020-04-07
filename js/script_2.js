60c3a65f339b0554394e73ef1b79db1ab3629603
asteain-ninia <asteain25@gmail.com>
2020-04-08 00:33:14 +0900

    うごいたのに動かなくなった。変数名がごっちゃになってる

--- js/script_2.js
+++ js/script_2.js
@@ -2,43 +2,61 @@ const electron = require('electron');
 const {ipcRenderer}=electron;
 const fs=require('fs')
 
-let entry=document.getElementById("entry");
+let wordID=document.getElementById("wordID");
+let spellingBox=document.getElementById("spellingBox")
+let tagBox=document.getElementById("tagBox");
+let charBox=document.getElementById("charBox")
 
 ipcRenderer.on('target',(event,arg)=>{
-    var target=arg.path
-    ShowWord(target);
+    var target_number=arg.number;
+    var target_path=arg.path;
+
+    console.log(arg);
+    load_word(target_path,target_number);
 });
 
-function ShowWord(target){
-    var data = fs.readFileSync(target, 'utf8') //pathの向こうにあるファイルをテキストで読む
+function load_word(target_path,target_number){
+    var data = fs.readFileSync(target_path, 'utf8') //pathの向こうにあるファイルをテキストで読む
     json = JSON.parse(data); //jsonでパース(ここ二行scrpt_1と共通)
 
-    //欄内の掃除は不要
+    var entry=json.words[target_number].entry
 
-    var wordID=document.getElementById("wordID");
-    var form=document.getElementById("spelling");
-    var tag=document.getElementById("tag");
-    var char=document.getElementById("char")
+    wordID.innerHTML="ID:"+target_number;
+
+    var forms_queue=entry.form.length;
+    for(let i=0; i<forms_queue; i++){
+        load_spelling(entry,i);
+    }
+
+    var chars_queue=entry.char.length;
+    for(let i=0; i<chars_queue;i++){
+        //load_chars(entry,i);
+    }
+
+
+
+    var tags_queue=entry.tags.length
 }
 
-let spellingFormCount=0;
-
-function add_spelling(){
-    spellingFormCount++;
-    var spellingBox=document.getElementById("spellingBox")
+function load_spelling(entry,i){
+    var custom=1　//1=spelling
 
     var spelling=document.createElement('form')
-    spelling.className="spelling";
-    spelling.id="spelling-"+spellingFormCount;
+    spelling.name="spelling-"+i;
+    spelling.className="input-1";
+    spelling.id="spelling-"+i;
 
     var spell=document.createElement('input');
     spell.type="text";
     spell.name="spell";
+    spell.value=entry.form[i]
+
     var remove=document.createElement('input');
     remove.type="button";
     remove.name="remove";
     remove.value="-"
-    remove.setAttribute("onclick","remove_spelling("+spellingFormCount+")")
+
+    remove.setAttribute("onclick","remove("+custom+","+i+")")
 
     spelling.appendChild(spell);
     spelling.appendChild(remove);
@@ -46,7 +64,39 @@ function add_spelling(){
     spellingBox.appendChild(spelling)
 }
 
-function remove_spelling(spelling_count){
-    var remove_target=document.getElementById("spelling-"+spelling_count)
-    spellingBox.removeChild(remove_target);
+let order=0
+function entry_add(custom){//空っぽの窓を末尾に追加する関数
+
+    var element=document.createElement('form')
+    element.name=custom+order;
+    element.className="input-1";
+    element.id=custom+order;
+
+    var column_value=document.createElement('input');//窓
+    column_value.type="text";
+    column_value.name="content";
+
+    var remove=document.createElement('input');//-ボタン
+    remove.type="button";
+    remove.name="remove";
+    remove.value="-"
+    remove.setAttribute("onclick","remove("+custom+","+order+")")
+
+    element.appendChild(column_value);
+    element.appendChild(remove);
+
+    spellingBox.appendChild(element)
+
+    order++;
+}
+
+function remove(custom,order){
+    //どの窓がremoveを行ったかを受け取って、それをですとろーい
+    var remove_target=document.getElementById(custom+"-"+order)
+    remove_target.parentNode.removeChild(remove_target);
+}
+
+function add_spelling(){
+    custom=1
+    entry_add(custom)
 }
