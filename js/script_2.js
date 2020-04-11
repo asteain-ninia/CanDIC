35016e2a2ec0ac69b414901a4b164ccb2d12a141
asteain-ninia <asteain25@gmail.com>
2020-04-11 15:36:35 +0900

    表示の改善・タグの試み

--- js/script_2.js
+++ js/script_2.js
@@ -2,15 +2,19 @@ const electron = require('electron');
 const {ipcRenderer}=electron;
 const fs=require('fs')
 
-let entry=null;
+let entry
 let FormID=0;
 let customID=0;
 let wordID=document.getElementById("wordID");
 let spellingBox=document.getElementById("spellingBox");
 let pronunBox=document.getElementById('pronunBox');
-let charBox=document.getElementById("charBox");
 let tagBox=document.getElementById("tagBox");
+let charBox=document.getElementById("charBox");
 
+let forms_queue
+let pronuns_queue
+let tags_queue
+let chars_queue
 
 
 ipcRenderer.on('target',(event,arg)=>{
@@ -24,22 +28,32 @@ ipcRenderer.on('target',(event,arg)=>{
 
 function load_word(target_number){
     if(target_number!=-1){
-        
-        //各データの長さチェック
-        entry=json.words[target_number].entry
-        var forms_queue=entry.form.length;
-        var pronuns_queue=entry.pronunciation.length;
         wordID.innerHTML="ID:"+target_number;
+
+        entry=json.words[target_number].entry
+        dictionary=json.dictionary
+
+        //各データの長さチェック
+        forms_queue=entry.form.length;
+        pronuns_queue=entry.pronunciation.length;
+        tags_queue=entry.tags.length
+        chars_queue=entry.char.length
 
         customID=1;//第一：語形の読み込み
         for(let i=0; i<forms_queue; i++){
             entry_load(customID,i);
         }
 
-        customID=2;
+        customID=2;//第二：撥音の読み込み
         for(let i=0; i<pronuns_queue;i++){
             entry_load(customID,i);
         }
+
+        customID=3;
+        for(let i=0;i<tags_queue;i++){
+            entry_load(customID,i);
+        }
+
 
     }else{//新規作成時の画面
         wordID.innerHTML="NEW WORD";
@@ -47,8 +61,7 @@ function load_word(target_number){
 }
 
 function entry_load(custom,i){
-    var element=document.createElement('form');
-    element.className="input-1";
+    var element=document.createElement('form');//form要素
     element.name=element.id=FormID;
 
     var column_value=document.createElement('input');//窓
@@ -65,16 +78,38 @@ function entry_load(custom,i){
         case 1:
             element.appendChild(column_value);
             element.appendChild(remove);
+            element.className="input-1";
             spellingBox.appendChild(element);
+            
             if(i!=-1)
                 {column_value.value=entry.form[i];}
             break;
         case 2:
             element.appendChild(column_value);
             element.appendChild(remove);
+            element.className="input-1";
             pronunBox.appendChild(element);
             if(i!=-1)
                 {column_value.value=entry.pronunciation[i];}
+            break;
+        case 3:
+            var tag=document.createElement('span');
+            tag.className="tag";
+            for(let j=0;j<tags_queue;j++){
+                var tagID=entry.tags[j]
+                var tags_queue_dictionary=dictionary.tags.length;
+                for(let k=0;k<tags_queue_dictionary;k++){
+                    if(dictionary.tags[k].id==tagID){
+                        var tagName=dictionary.tags[k].name;
+                    }
+                }
+            }
+            var tagDisplay = document.createTextNode(tagName);
+            tag.appendChild(tagDisplay);
+            element.appendChild(tag);
+            element.appendChild(remove);
+            element.className="input-2";
+            tagBox.appendChild(element);
             break;
     }
     FormID++
@@ -86,6 +121,6 @@ function remove(target){
     remove_target.parentNode.removeChild(remove_target);
 }
 
-function add_button(customID,i){
-    entry_load(customID,i)
+function add_button(customID){
+    entry_load(customID,-1)
 }
