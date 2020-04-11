e43a0ff130038b77e62efb4065ef395cdef1acb5
asteain-ninia <asteain25@gmail.com>
2020-04-11 17:10:43 +0900

    タグ追加の基礎、あともうちょい！

--- js/script_2.js
+++ js/script_2.js
@@ -9,13 +9,13 @@ let wordID=document.getElementById("wordID");
 let spellingBox=document.getElementById("spellingBox");
 let pronunBox=document.getElementById('pronunBox');
 let tagBox=document.getElementById("tagBox");
+let tagSelection=document.getElementById("tagSelection");
 let charBox=document.getElementById("charBox");
 
 let forms_queue
 let pronuns_queue
 let tags_queue
 let chars_queue
-
 
 ipcRenderer.on('target',(event,arg)=>{
     var target_number=arg.number;
@@ -25,6 +25,7 @@ ipcRenderer.on('target',(event,arg)=>{
     json = JSON.parse(data); //jsonでパース(ここ二行scrpt_1と共通)
     load_word(target_number);
 });
+
 
 function load_word(target_number){
     if(target_number!=-1){
@@ -39,6 +40,8 @@ function load_word(target_number){
         tags_queue=entry.tags.length
         chars_queue=entry.char.length
 
+        var tags_queue_dictionary=dictionary.tags.length;
+
         customID=1;//第一：語形の読み込み
         for(let i=0; i<forms_queue; i++){
             entry_load(customID,i);
@@ -49,7 +52,15 @@ function load_word(target_number){
             entry_load(customID,i);
         }
 
-        customID=3;
+        for(let k=0;k<tags_queue_dictionary;k++){//tag窓の生成
+            var tag_option=document.createElement('option');
+            tag_option.value=dictionary.tags[k].name;
+            var tag_option_content=document.createTextNode(dictionary.tags[k].name);
+            tag_option.appendChild(tag_option_content);
+            tagSelection.appendChild(tag_option)
+        }
+
+        customID=3;//第三：タグの読み込み
         for(let i=0;i<tags_queue;i++){
             entry_load(customID,i);
         }
@@ -95,20 +106,17 @@ function entry_load(custom,i){
         case 3:
             var tag=document.createElement('span');
             tag.className="tag";
-            for(let j=0;j<tags_queue;j++){
-                var tagID=entry.tags[j]
-                var tags_queue_dictionary=dictionary.tags.length;
-                for(let k=0;k<tags_queue_dictionary;k++){
-                    if(dictionary.tags[k].id==tagID){
-                        var tagName=dictionary.tags[k].name;
-                    }
+            var tagID=entry.tags[i]
+            var tags_queue_dictionary=dictionary.tags.length;
+            for(let k=0;k<tags_queue_dictionary;k++){
+                if(dictionary.tags[k].id==tagID){
+                    var tagName=dictionary.tags[k].name;
                 }
             }
             var tagDisplay = document.createTextNode(tagName);
             tag.appendChild(tagDisplay);
             element.appendChild(tag);
             element.appendChild(remove);
-            element.className="input-2";
             tagBox.appendChild(element);
             break;
     }
