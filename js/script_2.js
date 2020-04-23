a9935f343970f536148a0af72de7636301fb70c9
asteain-ninia <asteain25@gmail.com>
2020-04-23 18:18:36 +0900

    content_loadの全削除と若干の修正

--- js/script_2.js
+++ js/script_2.js
@@ -7,6 +7,7 @@ const fs=require('fs')
  contents=null
 
  customID=0;
+ 
  spellID=0;
  pronunID=0;
  charID=0;
@@ -63,14 +64,13 @@ function load_word(target_number){
         wordID.innerHTML="ID:"+target_number;
 
         entry=json.words[target_number].entry;
-        contents=json.words[target_number].contents
-
         //各データの長さチェック
         forms_queue=entry.form.length;
         pronuns_queue=entry.pronunciation.length;
         tags_queue=entry.tags.length;
         chars_queue=entry.char.length;
-
+        
+        contents=json.words[target_number].contents
         contents_queue=contents.length
 
         tag_show()
@@ -97,18 +97,7 @@ function load_word(target_number){
 
         //訳の読み込み
         for(let i=0;i<contents_queue;i++){
-
-            contents_column=document.createElement('div');
-            contents_column.name="contents_column";
-            contents_column.id="content"+contentID;
-            contents_column.className="contents_border"
-
-            content_customID=1;//品詞の読み込み
-            content_load(content_customID,i);
-
-            content_customID=2;//訳語の読み込み
-            content_load(content_customID,i)
-
+            content_load(i);
         }
 
     }else{//新規作成時の画面
@@ -213,142 +202,13 @@ function tag_switch(i){
     }
 }
 
-function content_load(content_customID,i){//地獄
-
-    switch(content_customID){
-        case 1:{//クラス情報設定
-            var class_select=document.createElement('select');
-            class_select.className="class";
-            class_select.id="class"+contentID;
-
-            for(let j=0;j<classes_queue_dictionary;j++){//option生成
-                    var class_option=document.createElement('option')
-                    class_option.text=dictionary.classes[j].name;
-                    class_option.value=dictionary.classes[j].id;
-                    class_select.appendChild(class_option)
-            }
-            
-            if(i!==-1){class_select.selectedIndex=contents[i].class;}//selectedIndexにデータにあるIDを代入
-            contents_column.appendChild(class_select);
-            contents_column.appendChild(document.createElement('hr'))
-            }break
-        case 2:{
-            var transID=0;
-
-            var trans_box=document.createElement('div');
-            trans_box.id="trans_box"
-            trans_box.className="contents_border"
-        
-            trans_queue=contents[i].trans.length;
-            for(let j=0;j<trans_queue;j++){//訳語窓生成
-
-                var trans_form=document.createElement('form');
-                trans_form.id="content"+contentID+"form"+transID;
-                trans_form.className="input-1";
-
-                trans_value=document.createElement('input');
-                trans_value.id="content"+contentID+"trans"+transID;
-                trans_value.className="large"
-
-                var remove=document.createElement('input');//-ボタン
-                remove.type="button";
-                remove.name="remove";
-                remove.value="-"
-
-                remove.setAttribute("onclick","remove('content"+contentID+"form"+transID+"')")
-                if(i!==-1){trans_value.value=contents[i].trans[j]}
-
-                trans_form.appendChild(trans_value)
-                trans_form.appendChild(remove)
-                trans_box.appendChild(trans_form)
-                transID++
-            }
-
-            var add_form=document.createElement('form')
-            add_form.className="input-1"
-
-            var add=document.createElement('input');
-            add.type="button";
-            add.name="add";
-            add.value="+";
-            add.setAttribute('onclick',"add_button(4)")
-
-            add_form.appendChild(document.createElement('a'))
-            add_form.appendChild(document.createElement('a'))
-            add_form.appendChild(add);
-            trans_box.appendChild(add_form);
 
 
-            contents_column.appendChild(trans_box)
-            contents_column.appendChild(add_form)
-            contents_column.appendChild(document.createElement('hr'))
-            }break
-    }
-
-
-
-    {
-//content_queue=contents[i].content.length;
-//     var content_texts_box=document.createElement('div');
-//     content_texts_box.id="content_texts_box"
-//     content_texts_box.className="contents_border"
-
-
-//     textID=0;
-//     contents_content_queue=contents[i].content.length
-//     for(let j=0;j<contents_content_queue;j++){//text情報設定
-//         shelf_maker(i,j);
-//         function shelf_maker(i,j){
-//             var content_shelf=document.createElement('div');
-//             content_shelf.id="content"+contentID+"shelf"+shelfID
-
-//             var title_select=document.createElement('select');
-//             title_select.className="title";
-//             title_select.id="content"+contentID+"title"+textID;
-//             for(let k=0;k<title_queue_dictionary;k++){//option生成
-//                 var title_option=document.createElement('option')
-//                 title_option.text=dictionary.titles[k].name;
-//                 title_option.value=dictionary.titles[k].id;
-//                 title_select.appendChild(title_option)
-//             }
-//             //selectedIndexにデータにあるIDを代入
-//             if(i!==-1){title_select.selectedIndex=contents[i].content[j].title;}
-
-//             var content_texts_text=document.createElement('textarea')
-//             content_texts_text.value=contents[i].content[j].text
-//             content_texts_text.id="content"+contentID+"text"+textID;
-//             content_texts_text.className="textBox"
-
-//             var remove=document.createElement('input');//-ボタン
-//             remove.type="button";
-//             remove.name="remove";
-//             remove.value="-"
-//             remove.setAttribute("onclick","remove('"+"content"+contentID+"shelf"+shelfID+"')")
-            
-//             content_shelf.appendChild(title_select);
-//             content_shelf.appendChild(document.createElement('br'));
-//             content_shelf.appendChild(content_texts_text)
-//             content_shelf.appendChild(remove)
-
-//             content_texts_box.appendChild(content_shelf)
-//             textID++
-//             shelfID++
-//         }
-//     }
-//    contents_column.appendChild(content_texts_box)
-//    contents_column.appendChild(document.createElement('hr'))
-    }
-
-
-
-    contentsBox.appendChild(contents_column);
-    contentID++
-}
 
 
 //どの窓がremoveを行ったかを受け取って、それをですとろーい
 function remove(target){
-    console.log(target)
+    console.log("remove"+target)
     var remove_target=document.getElementById(target)
     remove_target.parentNode.removeChild(remove_target);
 }
@@ -361,7 +221,8 @@ function add_button(customID){
             entry_load(customID,-1)
             break;
         case 4:
-            content_load(customID,-1)
+            
+            break;
     }
 }
 
