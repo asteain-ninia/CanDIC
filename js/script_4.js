f18b3f7f6e83d15e108d313e7b24a5e6024535a3
asteain-ninia <asteain25@gmail.com>
2020-05-14 09:02:23 +0900

    辞書編集：ガワの完成

--- js/script_4.js
+++ js/script_4.js
@@ -5,9 +5,16 @@ const fs = require('fs')
 function createElement(type){return document.createElement(type);}
 function createTextNode(value){return document.createTextNode(value);}
 
+let tagsID=0;
+let classesID=0;
+let titleID=0;
+
 let dictionary
 let language=document.getElementById("language")
 let tags=document.getElementById("tags")
+let classes=document.getElementById("classes")
+let titles=document.getElementById("titles")
+let meta=document.getElementById("meta")
 
 ipcRenderer.on('target',function(event,arg){
 	if(arg){
@@ -26,7 +33,10 @@ function show_edit_forms(path){
 
 	while (language.firstChild) {//language欄内の掃除
     	language.removeChild(language.firstChild);
-    }
+	}
+	while (meta.firstChild) {//meta欄内の掃除
+    	meta.removeChild(meta.firstChild);
+	}
 
 	if(dictionary.type=="TNN"){
 		console.log("filepath TNN reserved")
@@ -34,6 +44,13 @@ function show_edit_forms(path){
 		for(let i=0;i<dictionary.tags.length;i++){
 			createform(2,i);//tags欄
 		}
+		for(let i=0;i<dictionary.classes.length;i++){
+			createform(3,i);//classes欄
+		}
+		for(let i=0;i<dictionary.titles.length;i++){
+			createform(4,i);//titles欄
+		}
+		show_meta_info();
 	}else{
 		var Err_message=document.createTextNode("対応していない辞書形式のようです")
 		var Err_element=document.createElement("div")
@@ -63,15 +80,59 @@ function createform(customID,i){
 			label.appendChild(createTextNode("言語名"))
 			language.appendChild(label);
 
+			column_value.id="language"
 			column_value.value=dictionary.language
 
 			language.appendChild(createElement('br'));
 			language.appendChild(element);
 			break;
-		case 2://タグ欄
-		column_value.value=dictionary.tags[i].name
-		element.appendChild(remove);
-		tags.appendChild(element);
+		case 2://tags欄
+			element.id="tag"+tagsID+"Box"
+			column_value.id="tag"+tagsID
+			column_value.value=dictionary.tags[i].name
+
+			remove.setAttribute("onclick","remove('tag"+tagsID+"Box')")
+			element.appendChild(remove);
+
+			tags.appendChild(element);
+			tagsID++
+			break;
+		case 3://classes欄
+			element.id="class"+classesID+"Box"
+			column_value.id="class"+classesID
+			column_value.value=dictionary.classes[i].name
+
+			remove.setAttribute("onclick","remove('class"+classesID+"Box')")
+			element.appendChild(remove);
+
+			classes.appendChild(element);
+			classesID++
+			break;
+		case 4://titles欄
+			element.id="title"+titleID+"Box"
+			column_value.id="title"+titleID
+			column_value.value=dictionary.titles[i].name
+
+			remove.setAttribute("onclick","remove('title"+titleID+"Box')")
+			element.appendChild(remove);
+
+			titles.appendChild(element);
+			titleID++
 			break;
 	}
+}
+function show_meta_info(){
+	var meta_label=createTextNode("辞書情報");
+	var type_info=createTextNode(dictionary.type+"："+dictionary.version);
+	
+	meta.appendChild(meta_label)
+	meta.appendChild(createElement("br"))
+	meta.appendChild(type_info);
+}
+
+
+function remove(target){
+    console.log("removed:"+target)
+    var remove_target=document.getElementById(target)
+    remove_target.parentNode.removeChild(remove_target);
 }
